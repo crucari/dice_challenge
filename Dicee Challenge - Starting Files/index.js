bd53d7e0a28d91d3b84ec44f414eda90fb6298f4
Carina Cruz <lecruz@bu.edu>
2018-11-30 00:05:41 +0000

    player2 random dice switch working

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,1 +1,17 @@
-alert("Working!"); 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //produces a random # btw 1-
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice.png - dice.ong 
+
+var randomImageSource = "images/" + randomDiceImage; //images.dice.png - images.dice.png 
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource); 
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; 
+
+var image2 = document.querySelectorAll("img")[1]; 
+
+image2.setAttribute("src", randomImageSource);
