1f507ab4421b4ce9bd3886470a48abbd5d61a249
Carina Cruz <lecruz@bu.edu>
2018-11-30 00:11:58 +0000

    dynamic heading

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -15,3 +15,16 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1]; 
 
 image2.setAttribute("src", randomImageSource);
+
+//If player 1 wins 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+} 
+//if player 2 wins
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!"; 
+}
+//if the dice are equal 
+else {
+    document.querySelector("h1").innerHTML = "Draw!"; 
+}
